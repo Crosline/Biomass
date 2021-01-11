93104adc38a408b59af5c14f6f5ca66084172060
Yiğit <yighitgulec@gmail.com>
2021-01-11 23:34:28 +0300

    Character

--- public_html/app.js
+++ public_html/app.js
@@ -6,6 +6,8 @@ import {OBJLoader2} from './resources/threejs/r122/examples/jsm/loaders/OBJLoade
     function main() {
   /* ObJECTS */
   var player = new THREE.Object3D();
+  var trashbag = new THREE.Object3D();
+  var loggedKey;
   /* OBJECTS */
   const canvas = document.querySelector('#c');
   const renderer = new THREE.WebGLRenderer({canvas});
@@ -18,8 +20,8 @@ import {OBJLoader2} from './resources/threejs/r122/examples/jsm/loaders/OBJLoade
   camera.position.set(0, 15, 20);
 
   const controls = new OrbitControls(camera, canvas);
-  controls.target.set(0, 5, 0);
-  controls.update();
+  controls.target.set(camera.position.x, camera.position.y, camera.position.z);
+  
 
   const scene = new THREE.Scene();
   scene.background = new THREE.Color('cyan');
@@ -64,13 +66,24 @@ import {OBJLoader2} from './resources/threejs/r122/examples/jsm/loaders/OBJLoade
   }
 
   //
+  
     const objLoader = new OBJLoader2();
+    
       objLoader.load('resources/FinalBaseMesh.obj', (root) => {
       root.scale.set(0.5, 0.5, 0.5);
       root.rotation.y = Math.PI * -1;
       player.add(root);
       //scene.add(root);
     });
+    
+    const objLoaderx = new OBJLoader2();
+      objLoaderx.load('obj/trash/trash_bag.obj', (rootx) => {
+      rootx.scale.set(4,4,4);
+      rootx.position.set(0, 5, );
+      trashbag.add(rootx);
+      });
+         
+     
   //
 
   function resizeRendererToDisplaySize(renderer) {
@@ -83,7 +96,9 @@ import {OBJLoader2} from './resources/threejs/r122/examples/jsm/loaders/OBJLoade
     }
     return needResize;
   }
-
+  //
+    
+  //
   function render() {
 
     if (resizeRendererToDisplaySize(renderer)) {
@@ -93,8 +108,43 @@ import {OBJLoader2} from './resources/threejs/r122/examples/jsm/loaders/OBJLoade
     }
 
     scene.add(player);
+    scene.add(trashbag);
     
     
+    camera.position.z = player.position.z + 10;
+    camera.position.x = player.position.x;
+    
+    // camera.position.x = 0;
+    
+    canvas.addEventListener('keydown', (e) => {
+        loggedKey = e.keyCode;
+    });
+    
+    canvas.addEventListener('keyup', (e) => {
+        loggedKey = -1;
+    });
+    // forward
+    if(loggedKey == 87){
+        player.translateZ(-0.1);
+    }
+    // rear
+    if(loggedKey == 83){
+        player.translateZ(+0.1);
+    }
+    // right
+    if(loggedKey == 68){
+        player.rotation.y += -0.1;
+        camera.rotation.y = player.rotation.y;
+    }
+    if(loggedKey == 65){
+        player.rotation.y += 0.1;
+    }
+    
+    
+    
+    
+    controls.target.set(player.position.x, player.position.y + 10, player.position.z);
+    controls.update();
     renderer.render(scene, camera);
 
     requestAnimationFrame(render);
