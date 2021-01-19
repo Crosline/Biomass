085f8b4dfd707defdef972453b523d4cbf1360a8
ege akat <egeakat@hacettepe.edu.tr>
2021-01-19 23:05:20 +0300

    finished energy bar and made it dynamic also added tasks

--- public_html/ui.js
+++ public_html/ui.js
@@ -1,6 +1,18 @@
 var controlBoxToggleButton = document.getElementById("toggle-information-nav");
 var controlBoxContents = document.getElementById("control-information-box-wrapper");
+var toggleShaderButton = document.getElementById("toggle-shader-button");
+var increaseLighting = document.getElementById("plus");
+var decreaseLighting = document.getElementById("minus");
+var lightingAmount = document.getElementById("lighting-amount");
+var outerBar = document.getElementById("outer-bar");
+var innerBar = document.getElementById("inner-bar");
+
+var energyPercentage = document.getElementById("energy-amount");
+
 var toggleControlBox = true;
+var toggleShader = false 
+var totalEnergy = 200;
+var gameOver = false;
 
 controlBoxToggleButton.addEventListener("click", (event)=>{
     event.preventDefault();
@@ -16,3 +28,38 @@ controlBoxToggleButton.addEventListener("click", (event)=>{
     toggleControlBox = !toggleControlBox;
 })
 
+
+toggleShaderButton.addEventListener("click", (event)=>{
+    event.preventDefault();
+    toggleShader = !toggleShader;
+})
+
+increaseLighting.addEventListener("click", (event) => {
+    event.preventDefault();
+    if(lightingAmount.value < parseInt(lightingAmount.max)){
+        lightingAmount.value = parseInt(lightingAmount.value)+1;
+
+    }
+})
+
+decreaseLighting.addEventListener("click", (event) => {
+    event.preventDefault();
+    if(lightingAmount.value > parseInt(lightingAmount.min)){
+        lightingAmount.value = parseInt(lightingAmount.value)-1;
+
+    }
+})
+
+
+var energyInterval =  setInterval(function(){
+    if(totalEnergy <= 0){
+        gameOver = true;
+        clearInterval(energyInterval);
+    }
+    else{
+        totalEnergy--;
+        energyPercentage.innerHTML = totalEnergy / 2 + "%";
+        innerBar.style.width = parseInt(300 * (totalEnergy/200)) + "px"; 
+    
+    }
+}, 1000);
